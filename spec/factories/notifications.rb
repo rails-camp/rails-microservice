b75114b84670ede8e696c6d3d47c7a89a2d2c8a4
Jordan Hudgens <jordan@wow.com>
2016-02-13 14:17:29 -0600

    Implemented factory girl rails example

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :notification do
-    phone ""
-    body ""
-    source_app "MyString"
+    phone "5555555555"
+    body "My message"
+    source_app "some_app"
+  end
+
+  factory :second_notification, class: 'Notification' do
+    phone "5555555555"
+    body "My message"
+    source_app "some_app"
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe 'creation' do
     it 'can be created' do
-      notification = Notification.create(phone: "555-555-5555", body: "My message", source_app: "some_app")
+      notification = FactoryGirl.create(:notification)
       expect(notification).to be_valid
     end
   end
