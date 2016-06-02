15fec834652f5df3264d9fe1fbddd982a4be9442
Jordan Hudgens <jordan@wow.com>
2016-06-02 11:31:38 -0500

    Implemented request specs

--- spec/requests/notification_spec.rb
+++ spec/requests/notification_spec.rb
@@ -8,16 +8,33 @@ RSpec.describe Notification, type: :request do
     }
 
     post "/notifications",
-  	{
-    	notification: {
-	    	phone: "5555555555",
-	    	body: "My Message",
-	    	source_app: "my_app_name"
-  		}
-  	}, headers
+    {
+      notification: {
+        phone: "5555555555",
+        body: "My Message",
+        source_app: "my_app_name"
+      }
+    }, headers
 
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:created)
+  end
+
+  it 'renders an error status if the notification was not created' do
+    headers = {
+      "ACCEPT" => "application/json"
+    }
+
+    post "/notifications",
+    {
+      notification: {
+        phone: "5555555555",
+        body: "My Message"
+      }
+    }, headers
+
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:unprocessable_entity)
   end
 
 end
