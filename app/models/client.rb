5e5a9cc40b5629ff186c6e6ea2f46b0762698d8a
Jordan Hudgens <jordan@wow.com>
2016-07-07 15:30:14 -0500

    Implemented api key generator

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,12 @@ class Client < ActiveRecord::Base
   has_many :notifications, foreign_key: 'source_app', primary_key: 'source_app'
   validates_presence_of :source_app, :api_key
   validates_uniqueness_of :source_app, :api_key
+
+  before_validation :set_api_key
+
+  private
+
+    def set_api_key
+      self.api_key = ApiKey.generator
+    end
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -25,4 +25,11 @@ RSpec.describe Client, type: :model do
       expect(duplicate_client).to_not be_valid
     end
   end
+
+  describe 'callbacks' do
+    it 'will have an api_key automatically assigned when created' do
+      client = Client.create(source_app: "app_name")
+      expect(client.api_key).to_not be_nil
+    end
+  end
 end
